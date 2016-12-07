681c6ef7d366a2b24491d49a395a552f48ad4d4b
antizwiebel <Penedermark@gmail.com>
2016-12-07 14:23:54 +0100

    DONE

---
+++ src/fh/hagenberg/PenederMauler/CookieServer.java
@@ -0,0 +1,32 @@
+package fh.hagenberg.PenederMauler;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+/**
+ * Created by Mark on 07.12.2016.
+ */
+public class CookieServer
+        extends UnicastRemoteObject
+        implements IRemoteCookies
+{
+    public CookieServer() throws RemoteException {super();}
+
+    @Override
+    public String getCookie() throws RemoteException {
+        return "COOKIES!";
+    }
+
+    public static void main(String[] args) {
+        try {
+            CookieServer cookieServer= new CookieServer();
+            Registry registry= LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
+            registry.rebind("CookieServer", cookieServer);
+            System.out.println("Server is waiting for cookies!");
+        } catch (Exception _e) {
+            _e.printStackTrace();
+        }
+    }
+}
